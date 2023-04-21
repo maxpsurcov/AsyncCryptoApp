42e6d739ea8048fe6f8977069b0b357e1fb7c808
maxpsurcov <max.psurcov1@gmail.com>
2023-04-21 18:44:06 +0300

    Добавьте файлы проекта.

---
+++ AsyncCryptoApp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AsyncCryptoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBlowfish = new System.Windows.Forms.Button();
+            this.btnMD5 = new System.Windows.Forms.Button();
+            this.btnWake = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBlowfish
+            // 
+            this.btnBlowfish.Location = new System.Drawing.Point(120, 115);
+            this.btnBlowfish.Name = "btnBlowfish";
+            this.btnBlowfish.Size = new System.Drawing.Size(146, 63);
+            this.btnBlowfish.TabIndex = 0;
+            this.btnBlowfish.Text = "btnBlowfish";
+            this.btnBlowfish.UseVisualStyleBackColor = true;
+            // 
+            // btnMD5
+            // 
+            this.btnMD5.Location = new System.Drawing.Point(291, 115);
+            this.btnMD5.Name = "btnMD5";
+            this.btnMD5.Size = new System.Drawing.Size(146, 63);
+            this.btnMD5.TabIndex = 1;
+            this.btnMD5.Text = "btnMD5";
+            this.btnMD5.UseVisualStyleBackColor = true;
+            // 
+            // btnWake
+            // 
+            this.btnWake.Location = new System.Drawing.Point(471, 115);
+            this.btnWake.Name = "btnWake";
+            this.btnWake.Size = new System.Drawing.Size(146, 63);
+            this.btnWake.TabIndex = 2;
+            this.btnWake.Text = " btnWake";
+            this.btnWake.UseVisualStyleBackColor = true;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(173, 250);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 29);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 476);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnWake);
+            this.Controls.Add(this.btnMD5);
+            this.Controls.Add(this.btnBlowfish);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBlowfish;
+        private System.Windows.Forms.Button btnMD5;
+        private System.Windows.Forms.Button btnWake;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ AsyncCryptoApp/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsyncCryptoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void btnBlowfish_Click(object sender, EventArgs e)
+        {
+            string input = "Secret message";
+            string key = "MySecretKey";
+            byte[] encrypted = await Task.Run(() => EncryptBlowfish(input, key));
+            string decrypted = await Task.Run(() => DecryptBlowfish(encrypted, key));
+            txtResult.Text = decrypted;
+        }
+
+        private async void btnMD5_Click(object sender, EventArgs e)
+        {
+            string input = "Secret message";
+            byte[] hash = await Task.Run(() => HashMD5(input));
+            txtResult.Text = Convert.ToBase64String(hash);
+        }
+
+        private async void btnWake_Click(object sender, EventArgs e)
+        {
+            string input = "Secret message";
+            byte[] key = Encoding.UTF8.GetBytes("MySecretKey");
+            byte[] iv = Encoding.UTF8.GetBytes("MySecretIV");
+            byte[] encrypted = await Task.Run(() => EncryptWake(input, key, iv));
+            string decrypted = await Task.Run(() => DecryptWake(encrypted, key, iv));
+            txtResult.Text = decrypted;
+        }
+        private byte[] EncryptBlowfish(string input, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            using (var cipher = new BlowfishManaged())
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var encryptor = cipher.CreateEncryptor(keyBytes, null);
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(inputBytes, 0, inputBytes.Length);
+                    cs.FlushFinalBlock();
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        private string DecryptBlowfish(byte[] input, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using (var cipher = new BlowfishManaged())
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var decryptor = cipher.CreateDecryptor(keyBytes, null);
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(input, 0, input.Length);
+                    cs.FlushFinalBlock();
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+
+        private byte[] HashMD5(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                return md5.ComputeHash(inputBytes);
+            }
+        }
+
+        private byte[] EncryptWake(string input, byte[] key, byte[] iv)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            using (var cipher = new WakeManaged())
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var encryptor = cipher.CreateEncryptor(key, iv);
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(inputBytes, 0, inputBytes.Length);
+                    cs.FlushFinalBlock();
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        private string DecryptWake(byte[] input, byte[] key, byte[] iv)
+        {
+            using (var cipher = new WakeManaged())
+            using (var ms = new System.IO.MemoryStream())
+            {
+                var decryptor = cipher.CreateDecryptor(key, iv);
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(input, 0, input.Length);
+                    cs.FlushFinalBlock();
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+        }
+    }
+}
+
+
+
